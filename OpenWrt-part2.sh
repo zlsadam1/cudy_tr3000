159bb03196f74e8db78761e6c9cd39e2bfefffe5
Nova <piaomanor@gmail.com>
2025-03-09 12:18:03 +0800

    Update OpenWrt-part2.sh

--- OpenWrt-part2.sh
+++ OpenWrt-part2.sh
@@ -18,8 +18,12 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 修改时区(貌似没有起作用)
 #sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 #sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改 分区大小，默认 mod 分区大小为 112MB：0x7000000。改为 114MB：0x7200000
+sed -i '/label = "ubi"/{n;s/reg = <0x5c0000 0x[0-9a-f]\+>/reg = <0x5c0000 0x7200000>/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dts 
 
 # 报错修复
 ln -s /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
