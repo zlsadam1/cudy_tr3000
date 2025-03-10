2bbfb9e04c08483cee92ac2c58f56e5aa755849a
Nova <piaomanor@gmail.com>
2025-03-10 14:47:56 +0800

    Update OpenWrt-part1.sh

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -11,14 +11,14 @@
 #
 
 # upgrade gcc11
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install gcc-11 g++-11 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
-sudo update-alternatives --config gcc
-gcc --version
+# sudo apt-get update && sudo apt-get upgrade -y
+# sudo apt-get install software-properties-common -y
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+# sudo apt-get update
+# sudo apt-get install gcc-11 g++-11 -y
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+# sudo update-alternatives --config gcc
+# gcc --version
 
 # 自定义函数
 addFeeds(){
