600536d4c0a989e4021ccb2b206b98d235e21010
Nova <piaomanor@gmail.com>
2025-04-09 22:10:24 +0800

    Update preset-terminal-tools.sh

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir -p files/root
-mkdir -p files/usr/bin
 pushd files/root
 
 # Clone oh-my-zsh repository
@@ -12,17 +11,27 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zs
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 
+# Get .zshrc dotfile
+cp $GITHUB_WORKSPACE/scripts/.zshrc .
+
+popd
+
+
+mkdir -p files/usr/bin
+pushd files/usr/bin
+
 # 获取最新版本号
 LATEST_VERSION=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 # 下载压缩包
 curl -LO https://github.com/XTLS/Xray-core/releases/download/v$LATEST_VERSION/Xray-linux-arm64-v8a.zip
-# 解压缩压缩包
-unzip Xray-linux-arm64-v8a.zip
+
+# 解压缩压缩包到指定目录
+unzip Xray-linux-arm64-v8a.zip -d Xray-linux-arm64-v8a
+
 # 移动 xray 文件到 /usr/bin 目录
-ls ../user/bin/
-mv xray ../usr/bin/
-chmod +x ../usr/bin/xray
-ls ../user/bin/
+mv Xray-linux-arm64-v8a/xray .
+
+ls ./
 # 删除解压后的目录（假设解压后目录名为 Xray-linux-arm64-v8a）
 rm -rf Xray-linux-arm64-v8a
 # 清理下载的压缩包
