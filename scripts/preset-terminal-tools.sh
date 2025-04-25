a2e6411f4ce2757e4ceffd029e3b10b446193fa1
Nova <piaomanor@gmail.com>
2025-04-25 11:20:01 +0800

    Update preset-terminal-tools.sh

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -7,9 +7,11 @@ pushd files/root
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
 
 # Install extra plugins
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+#git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+#git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+#git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+
+rm -rf ./.oh-my-zsh/.git
 
 # Get .zshrc dotfile
 cp $GITHUB_WORKSPACE/scripts/.zshrc .
