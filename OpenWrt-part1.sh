fe5e4bd0c139bfa8bec606281146ba50513a4104
Nova <piaomanor@gmail.com>
2026-03-13 10:18:34 +0800

    Update feeds.conf.default with Passwall packages
    Add Passwall package feeds to feeds.conf.default.

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -47,6 +47,12 @@ function git_sparse_clone() {
 addFeeds custom https://github.com/kenzok8/openwrt-packages.git
 addFeeds small https://github.com/kenzok8/small.git
 
+# top install
+sed -i '1i\
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main\
+src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+
+
 # 支持 turboacc
 # 不带 shortcut-fe
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
