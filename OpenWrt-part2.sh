e1a8c7eb9f98467f3264636f1357015cd73f5f99
Nova <piaomanor@gmail.com>
2026-07-31 09:24:56 +0800

    Modify device name and set default theme
    Add build date identifier to LuCI status module.

--- OpenWrt-part2.sh
+++ OpenWrt-part2.sh
@@ -29,6 +29,9 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # 修改设备名称
 sed -i 's/OpenWrt/Cudy_TR3000/g' package/base-files/files/bin/config_generate
 
+#添加编译日期标识
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $BUILD_MARK-$BUILD_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
